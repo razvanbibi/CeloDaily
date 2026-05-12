187fc8c0c772e4465d38bc4ea84dd9ccc23c8d2d
Rayhan <mstrazvanbibi@gmail.com>
2026-05-12 23:57:12 +0600

    Removing some unnessary codes from api section

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,23 +1,5 @@
 import { Redis } from "@upstash/redis";
-
-const redis = Redis.fromEnv();
-
 export async function POST(req: Request) {
-
   const body = await req.json();
-
-  if (body.type === "frame_notification") {
-
-    const fid = body.data.fid;
-    const token = body.data.notification_token;
-
-    await redis.set(
-      `basedaily:notificationToken:${fid}`,
-      token
-    );
-
-  }
-
   return new Response("ok");
-
 }
