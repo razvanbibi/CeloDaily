ee99eac0de3737e4e81cfc09446cc2e02c5a38fb
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-13 08:03:51 +0600

    Remove unnecessary newline in getProfiles function

--- src/lib/profileStore.ts
+++ src/lib/profileStore.ts
@@ -34,7 +34,6 @@ export async function getProfile(address: string) {
   };
 }
 export async function getProfiles(addresses: string[]) {
-
   const profiles = await Promise.all(
 
     addresses.map(async (address) => {
